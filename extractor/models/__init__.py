2631d07a51ec2349a3da48ed6d6b5b8c4ca95a30
khagendra01 <khagendrakhatri365@gmail.com>
2025-11-12 15:30:15 -0500

    model change

--- extractor/models/__init__.py
+++ extractor/models/__init__.py
@@ -5,7 +5,6 @@ Model loading and management module.
 from extractor.models.blip_loader import BLIPLoader
 from extractor.models.whisper_loader import WhisperLoader
 from extractor.models.yolo_loader import YOLOLoader
-from extractor.models.ofa_loader import OFALoader
 
-__all__ = ['BLIPLoader', 'WhisperLoader', 'YOLOLoader', 'OFALoader']
+__all__ = ['BLIPLoader', 'WhisperLoader', 'YOLOLoader']
 
